fda1da8bf68916b32eefa54df592b12aad8c6c11
memelamc <mmemela@csir.co.za>
2021-03-16 10:09:19 +0200

    User Model

---
+++ Task_01/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_01.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string EmailAddress { get; set; }
+        public string Gender { get; set; }
+        public string IpAddress { get; set; }
+
+    }
+}
